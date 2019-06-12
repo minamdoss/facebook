28ce359a652ad15c43fa6fc39d09284fa1c49259
ZiadMaged <Ziad_7@live.com>
2019-06-12 19:03:32 +0200

    Update app

--- app.py
+++ app.py
@@ -44,11 +44,11 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = get_message()
+                    # response_sent_text = get_message()
                     nlu.setQuery(message['message'].get('text'),recipient_id)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
+                    # response_sent_nontext = get_message()
                     # send_message(recipient_id, response_sent_nontext)
                     nlu.setQuery(message['message'].get('attachments'),recipient_id)
     return "Message Processed"
